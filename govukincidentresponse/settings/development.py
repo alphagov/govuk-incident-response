c914a2b7a47cbb76317cb792202cb30bb6f44bcd
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-07-09 08:21:10 +0100

    Add Slack and incident settings

--- govukincidentresponse/settings/development.py
+++ govukincidentresponse/settings/development.py
@@ -1,1 +1,25 @@
+import os
+
 from .common import *
+
+
+SECRET_KEY = '&sr(f(17t^wrlx)d01j2zbu_8zp2c@bag=t_zky9h!8*j^g-xm'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
+    }
+}
+
+SITE_URL = os.environ.get('SITE_URL', 'http://localhost:8000')
+
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+SLACK_SIGNING_SECRET = os.environ.get('SLACK_SIGNING_SECRET')
+
+INCIDENT_CHANNEL_ID = os.environ.get('INCIDENT_CHANNEL_ID')
+INCIDENT_BOT_ID = os.environ.get('INCIDENT_BOT_ID')
--- govukincidentresponse/settings/production.py
+++ govukincidentresponse/settings/production.py
@@ -1,1 +1,21 @@
+import os
+
+import dj_database_url
+
 from .common import *
+
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {'default': dj_database_url.config()}
+
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SITE_URL = os.environ['SITE_URL']
+
+SLACK_TOKEN = os.environ['SLACK_TOKEN']
+SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
+
+INCIDENT_CHANNEL_ID = os.environ['INCIDENT_CHANNEL_ID']
+INCIDENT_BOT_ID = os.environ['INCIDENT_BOT_ID']
